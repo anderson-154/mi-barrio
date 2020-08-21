202d9ade48b833ad4f61636a3fe216a943d0d5f4
anderson-154 <54720103+anderson-154@users.noreply.github.com>
2020-08-21 12:46:56 -0500

    test

---
+++ test/model/StoreControlTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StoreControlTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
